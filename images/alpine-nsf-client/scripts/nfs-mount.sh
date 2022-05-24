232e0b9a1662b3448a1aa69642a608f161cc5160
Varun Sridharan <varunsridharan23@gmail.com>
2022-05-24 07:52:33 +0530

    Update nfs-mount.sh

--- images/alpine-nsf-client/scripts/nfs-mount.sh
+++ images/alpine-nsf-client/scripts/nfs-mount.sh
@@ -9,3 +9,5 @@ for entry in $( echo "$NFS_SERVERS" | tr "," "\n" ); do
   mount -t nfs4 ${SERVER[0]}:${SERVER[1]} "/mnt/shares/${SERVER[2]}"
   echo "NFS ${SERVER[0]} Mounted @ /mnt/shares/${SERVER[2]}"
 done
+
+rpcbind
